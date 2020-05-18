ff0545cccd95a46e861bad753316a99817bd7524
Timofey <timofei.chunarev@gmail.com>
2020-05-18 13:33:09 +0700

    burger

--- script/script.js
+++ script/script.js
@@ -27,15 +27,56 @@ document.addEventListener('DOMContentLoaded', function() {
     const questionTitle = document.querySelector('#question'); // Получаем h5 c id = "question".
     const formAnswers = document.querySelector('#formAnswers'); // Получем форму ответа formAnswers.
 
-  
+
+    // Бургер 
+
+    const burgerBtn = document.getElementById("burger"); // Получаем бургер по Id.
+    let clientWidth = document.documentElement.clientWidth; // В переменную clientWidth записываем ширину окна пользователя. 
+    // В объекте documentElement хранится весь html. А clientWidth это свойство documentElement, а именно ширина окна.
+    // console.dir('document.documentElement: ', document.documentElement); // Выводит documentElement в консоль в виде объекта, у которого можно посмотреть все его свойства.
+
+    // Условие скрывающее кнопку-бургер при загрузке страницы 
+    if (clientWidth < 768) {
+        burgerBtn.style.display = "flex";
+    } else {
+        burgerBtn.style.display = "none";
+    }
+
+    window.addEventListener('resize', function () { // Событие resize позволяет отслеживать изменения окна
+        clientWidth = document.documentElement.clientWidth; // Возвращает ширину экрана (clientWidth - ширина окна клиента)
+    
+        // Условие адаптивности
+        if(clientWidth < 768) {
+            burgerBtn.style.display = "flex"; // Обращаемся к стилям бургера и добавляет ему свойство css display: flex.
+        } else {
+            burgerBtn.style.display = "none"; // Обращаемся к стилям бургера и скрываем его с помошься свойства css display: none.
+        }
+    }); 
+
+
+    // Вешаем событие по клику на кнопку бургер. 
+    burgerBtn.addEventListener("click", function () {
+        modalBlock.classList.add('d-block'); // Добавляем класс 'd-block' из библиотеки bootstrap действие которого аналогично свойству CSS 'display: block';
+        playTest(); // Вызов функции тестирования
+
+        burgerBtn.classList.add("active");
+    });
+    
     // Навешиваем на кнопку обработчик событий 'click', по клику будет срабатывать функция 
     btnOpenModal.addEventListener('click', () => {
         modalBlock.classList.add('d-block'); // Добавляем класс 'd-block' из библиотеки bootstrap действие которого аналогично свойству CSS 'display: block';
         playTest(); // Вызов функции тестирования
     });
 
+    // Обработчик событий по клку, навешенный на кнопку-крестик, которая закрывает модальное окно
     closeModal.addEventListener('click', () => {
-        modalBlock.classList.remove('d-block');
+        modalBlock.classList.remove('d-block'); // Убирает класс 'd-block' у модольного окана, что скрывает его.
+        burgerBtn.classList.remove("active"); // Убирает класс 'activ' по нажатию на закрывающий модальное окно крестик
+
+        if(burgerBtn.classList.contains("active")) {
+            
+        }
+        
     });
 
     // Функция, включающая в себя весь функционал тестирования.
@@ -51,16 +92,19 @@ document.addEventListener('DOMContentLoaded', function() {
                     <span>Стандарт</span>
                     </label>
                 </div>
-                <div class="answers-item d-flex justify-content-center">
-                    <input type="radio" id="answerItem2" name="answer" class="d-none">
-                    <label for="answerItem2" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="./image/burgerBlack.png" alt="burger">
-                    <span>Черный</span>
-                    </label>
-                </div>
             `
         };
         renderQuestions();
     };
 
+
+
+
+
+
+
 });
+
+
+
+
